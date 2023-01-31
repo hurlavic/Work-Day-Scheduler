627e97bc7e5087f9fc91aac24c222109dbe32fd3
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-01-31 16:23:14 +0000

    "added codes to js file"

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+let currentTime = moment().hour();
+
+let firstHour = 8;
+let lastHour = 6;
+let textInput = document.getElementsByTagName('textarea');
+let container = document.querySelector('.container');
+let save = document.querySelector('.saveBtn');
