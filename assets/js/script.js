bbddab1dcc0a1b09be8404d2c778a26d94463f80
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-01-31 17:54:05 +0000

    "added logic statement to js"

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,23 @@ var timeBlock = document.querySelector('.time-block');
 document.getElementById('currentDay').innerHTML = currentDate;
 
 save.addEventListener('click', (e) => {
-    var clickedButton = $(clkBtn);
+    var clickedButton = $(this);
     var timeBlock2 = clickedButton.closest('.time-block');
     var closestTextArea = clickedButton.siblings('textarea');
-    var timeBlockId = timeBlockEl.attr("id");
-    localStorage.setItem(timeBlockId, closestTextArea.val());
+    var timeBlockId = timeBlock2.attr("id");
+    localStorage.setItem(timeBlockId, closestTextArea.value);
 });
 
+
+for (let i = 0; i<timeBlock.length; i++) {
+    var colorBoxes = timeBlock[i];
+//  var colorBoxes = parseInt($(this).attr("id").replace("hour", ""));
+ if (colorBoxes > currentTime) {
+    $(this).addClass("future");
+  } else if (colorBoxes < currentTime) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("present");
+  }
+}
 
